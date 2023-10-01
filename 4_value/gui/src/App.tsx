5b9f7db7e77a3c4112823c2e00e898b9d179a349
Audrey Santoso <104437268+fruitiecutiepie@users.noreply.github.com>
2023-10-01 17:05:59 +1100

    make tabs browser-compatible/responsive

--- 4_value/gui/src/App.tsx
+++ 4_value/gui/src/App.tsx
@@ -1,5 +1,7 @@
 import type { Component } from 'solid-js';
 import { Routes, Route, A } from '@solidjs/router';
+
+import logo from '../../../1_problem/assets/icon.svg'
 import Home from './pages/Home';
 import Routine from './pages/Routine';
 import New from './pages/New';
@@ -8,58 +10,130 @@ import Profile from './pages/Profile';
 
 const App: Component = () => {
   return (
-    <>
-      <nav class="flex w-full h-14 fixed bottom-0 space-x-12 bg-white justify-center">
-        <A
-          href="/"
-          class="material-symbols-outlined self-center"
-          activeClass="material-symbols-filled"
-          noScroll
+    <div>
+      <div
+        class="flex w-full h-14 fixed bottom-0 justify-center z-20 border-t
+          md:border-t-0 md:justify-end md:border-r md:top-0 md:bottom-auto md:overflow-y-auto md:h-full md:w-auto lg:w-1/4"
+      >
+        <div
+          class="flex justify-end md:flex-col md:space-y-12 md:justify-start md:items-center md:w-16 lg:w-52"
         >
-          home
-        </A>
-        <A 
-          href="/routine" 
-          class="material-symbols-outlined self-center" 
-          activeClass="material-symbols-filled"
-          noScroll
-        >
-          routine
-        </A>
-        <A 
-          href="/new" 
-          class="material-symbols-outlined self-center" 
-          activeClass="material-symbols-filled"
-        >
-          add_circle
-        </A>
-        <A 
-          href="/analytics" 
-          class="material-symbols-outlined self-center" 
-          activeClass="material-symbols-filled"
-          noScroll
-        >
-          analytics
-        </A>
-        <A 
-          href="/profile" 
-          class="material-symbols-outlined self-center" 
-          activeClass="material-symbols-filled"
-          noScroll
-        >
-          person
-        </A>
-      </nav>
-      <div class="m-5 my-10 mt-20">
-        <Routes>
-          <Route path="/" component={Home} />
-          <Route path="/routine" component={Routine} />
-          <Route path="/new" component={New} />
-          <Route path="/analytics" component={Analytics} />
-          <Route path="/profile" component={Profile} />
-        </Routes>
+          <div class="mt-12 hidden md:flex lg:self-start">
+            <A
+              href="/home"
+            >
+              <img src={logo} class="w-7 h-7" alt="logo" />
+            </A>
+          </div>
+          <nav
+            class="flex space-x-12 items-center
+            md:flex-col md:space-y-12 md:space-x-0 md:justify-start lg:items-start lg:self-start"
+          >
+            <A
+              href="/home"
+              class="flex items-center justify-center lg:space-x-2 lg:justify-start lg:w-full"
+              activeClass="material-symbols-filled"
+              noScroll
+            >
+              <span
+                class="material-symbols-outlined self-center"
+              >
+                home
+
+              </span>
+              <span
+                class="text-xl hidden lg:flex"
+              >
+                Home
+              </span>
+            </A>
+            <A
+              href="/routine"
+              class="flex items-center justify-center lg:space-x-2 lg:justify-start lg:w-full"
+              activeClass="material-symbols-filled"
+              noScroll
+            >
+              <span
+                class="material-symbols-outlined self-center"
+              >
+                routine
+
+              </span>
+              <span
+                class="text-xl hidden lg:flex"
+              >
+                Routine
+              </span>
+            </A>
+            <A
+              href="/new"
+              class="flex items-center justify-center lg:space-x-2 lg:justify-start lg:w-full"
+              activeClass="material-symbols-filled"
+            >
+              <span
+                class="material-symbols-outlined self-center"
+              >
+                add_circle
+
+              </span>
+              <span
+                class="text-xl hidden lg:flex"
+              >
+                Add
+              </span>
+            </A>
+            <A
+              href="/analytics"
+              class="flex items-center justify-center lg:space-x-2 lg:justify-start lg:w-full"
+              activeClass="material-symbols-filled"
+              noScroll
+            >
+              <span
+                class="material-symbols-outlined self-center"
+              >
+                analytics
+
+              </span>
+              <span
+                class="text-xl hidden lg:flex"
+              >
+                Analytics
+              </span>
+            </A>
+            <A
+              href="/profile"
+              class="flex items-center justify-center lg:space-x-2 lg:justify-start lg:w-full"
+              activeClass="material-symbols-filled"
+            >
+              <span
+                class="material-symbols-outlined self-center"
+              >
+                person
+
+              </span>
+              <span
+                class="text-xl hidden lg:flex"
+              >
+                Profile
+              </span>
+            </A>
+          </nav>
+        </div>
       </div>
-    </>
+      <div
+        class="md:left-20 lg:left-1/4 relative h-screen w-screen"
+      >
+        <div>
+          <Routes>
+            <Route path="/home" component={Home} />
+            <Route path="/routine" component={Routine} />
+            <Route path="/new" component={New} />
+            <Route path="/analytics" component={Analytics} />
+            <Route path="/profile" component={Profile} />
+          </Routes>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- 4_value/gui/src/pages/Home.tsx
+++ 4_value/gui/src/pages/Home.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <>
-      <div>
+      <div
+        class="p-5 py-10 pt-20"
+      >
         <h1>Welcome to your homepage!</h1>
       </div>
     </>
