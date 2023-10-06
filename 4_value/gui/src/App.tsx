27f9438f9de0923cda6f1ddcb18bb8ac8220fa3f
Audrey Santoso <104437268+fruitiecutiepie@users.noreply.github.com>
2023-10-06 20:05:40 +1100

    factorise Tabs and improve layout

--- 4_value/gui/src/App.tsx
+++ 4_value/gui/src/App.tsx
@@ -1,9 +1,5 @@
 import type { Component } from 'solid-js';
-import { Routes, Route, A, useLocation } from '@solidjs/router';
-
-import logo from '../../../1_problem/assets/icon.svg'
-import profile_picture from '../../../1_problem/assets/profile_picture.png'
-import TabButton from './components/TabButton';
+import { Routes, Route, useLocation } from '@solidjs/router';
 
 import Home from './pages/Home';
 import Routine from './pages/Routine';
@@ -14,95 +10,23 @@ import Profile from './pages/Profile';
 import Storage from './pages/Storage';
 import Archive from './pages/Archive';
 import Settings from './pages/Settings';
+import Tabs from './components/Tabs';
 
 const App: Component = () => {
   const location = useLocation();
   return (
     <div
-      class="flex flex-col"
+      class="flex flex-col md:flex-row justify-center h-screen"
     >
-      {location.pathname !== '/scan' && (
-        <div
-          class="flex w-full fixed min-h-[3.5rem] bg-zinc-50 bottom-0 z-10 border-t md:p-0
-            md:border-t-0 md:border-r lg:border-r-0 md:justify-end md:top-0 md:bottom-auto md:overflow-y-auto md:h-full md:w-auto lg:w-1/4"
-        >
-          <div
-            class="flex w-full justify-center items-end md:flex-col md:justify-between md:pb-12"
-          >
-            <div
-              class="flex justify-end items-center select-none md:flex-col md:justify-start md:items-center md:w-16 lg:w-52"
-            >
-              <div class="pt-3 hidden w-full md:flex lg:justify-start">
-                <A
-                  class="flex items-center justify-center py-3 px-5 md:px-3 my-1 mx-1 md:mx-0 lg:justify-start w-full"
-                  href="/home"
-                >
-                  <img src={logo} class="w-7 h-7" alt="logo" />
-                </A>
-              </div>
-              <nav
-                class="flex items-center justify-center w-full md:flex-col lg:items-start"
-              >
-                <TabButton path="/home" icon="home" label="Home" noScroll />
-                <TabButton path="/routine" icon="routine" label="Routine" noScroll />
-                <div
-                  class="flex-col hidden md:flex md:justify-start lg:items-start lg:self-start"
-                >
-                  <TabButton path="/items/storage" icon="home_storage" label="Storage" />
-                  <TabButton path="/items/collection" icon="shelves" label="Collection" />
-                  <TabButton path="/items/archive" icon="archive" label="Archive" />
-                </div>
-                <div
-                  class="flex md:hidden"
-                >
-                  <TabButton path="/items/collection" icon="shelves" label="Items" />
-                </div>
-                <TabButton path="/notifications" icon="notifications" label="Notifications" noScroll />
-                <A
-                  href="/profile"
-                  class="flex items-center justify-center py-3 px-5 md:px-3 my-1 mx-1 md:mx-0 lg:space-x-4 lg:justify-start w-full"
-                  activeClass="material-symbols-filled"
-                >
-                  <span
-                    class="material-symbols-outlined self-center"
-                  >
-                    person
-                  </span>
-                  {/* TODO */}
-                  {/* <img src={profile_picture} class="w-6 h-6 rounded-full" alt="profile_picture" /> */}
-                  <span
-                    class="text-xl font-display hidden lg:flex"
-                  >
-                    Profile
-                  </span>
-                </A>
-                <A
-                  href="/scan"
-                  class="
-                    hidden items-center justify-center rounded-full text-white bg-teal-400 shadow-[0_8px_28px_rgba(0,0,0,0.08)]
-                    md:flex md:w-12 md:h-12 lg:w-auto md:p-1 md:my-4 lg:py-3 lg:px-16 hover:bg-teal-500 transition duration-300
-                  "
-                  activeClass="material-symbols-filled"
-                >
-                  <span
-                    class="material-symbols-outlined self-center lg:hidden
-                    "
-                  >
-                    barcode_scanner
-                  </span>
-                  <span
-                    class="text-xl font-display hidden lg:flex"
-                  >
-                    Scan
-                  </span>
-                </A>
-              </nav>
-            </div>
-          </div>
-        </div>
-      )}
       <div
-        class="self-center min-h-screen w-full md:w-3/5 lg:w-1/2 lg:border-l lg:border-r"
+        class="sticky top-0 bottom-0 w-full order-2 md:order-1 md:w-auto lg:w-1/4"
+      >
+        {location.pathname !== '/scan' && (
+          <Tabs />
+        )}
+      </div>
+      <div
+        class="self-center w-full overflow-y-auto order-1 md:order-2 md:mx-5 md:w-3/5 lg:w-1/2 md:border-l md:border-r"
       >
         <div>
           <Routes>
@@ -117,6 +41,10 @@ const App: Component = () => {
             <Route path="/settings" component={Settings} />
           </Routes>
         </div>
+      </div>
+      <div
+        class="lg:w-1/4 lg:order-3"
+      >
       </div>
     </div>
   );
---
+++ 4_value/gui/src/components/Tabs.tsx
@@ -0,0 +1,87 @@
+import { A } from '@solidjs/router';
+import logo from '../../../../1_problem/assets/icon.svg';
+import TabButton from './TabButton';
+
+export default function Tabs() {
+  return (
+    <div
+      // class="flex w-full min-h-[3.5rem] bg-zinc-50 bottom-0 border-t
+      //   md:border-t-0 md:border-r lg:border-r-0 md:justify-end md:top-0 md:bottom-auto md:overflow-y-auto md:h-full md:w-auto lg:w-1/4"
+        class="sticky top-0 bottom-0 bg-zinc-50 border-t md:border-t-0"
+    >
+      <div
+        class="flex w-full justify-center items-end md:flex-col md:justify-between md:pb-12"
+      >
+        <div
+          class="flex justify-end items-center select-none md:flex-col md:justify-start md:items-center"
+        >
+          <div class="pt-3 hidden w-full md:flex lg:justify-start">
+            <A
+              class="flex items-center justify-center py-3 px-5 md:px-3 my-1 mx-1 md:mx-0 lg:justify-start w-full"
+              href="/home"
+            >
+              <img src={logo} class="w-7 h-7" alt="logo" />
+            </A>
+          </div>
+          <nav
+            class="flex items-center justify-center w-full md:flex-col lg:items-start"
+          >
+            <TabButton path="/home" icon="home" label="Home" noScroll />
+            <TabButton path="/routine" icon="routine" label="Routine" noScroll />
+            <div
+              class="flex-col hidden md:flex md:justify-start lg:items-start lg:self-start"
+            >
+              <TabButton path="/items/storage" icon="home_storage" label="Storage" />
+              <TabButton path="/items/collection" icon="shelves" label="Collection" />
+              <TabButton path="/items/archive" icon="archive" label="Archive" />
+            </div>
+            <div
+              class="flex md:hidden"
+            >
+              <TabButton path="/items/collection" icon="shelves" label="Items" />
+            </div>
+            <TabButton path="/notifications" icon="notifications" label="Notifications" noScroll />
+            <A
+              href="/profile"
+              class="flex items-center justify-center py-3 px-5 md:px-3 my-1 mx-1 md:mx-0 lg:space-x-4 lg:justify-start w-full"
+              activeClass="material-symbols-filled"
+            >
+              <span
+                class="material-symbols-outlined self-center"
+              >
+                person
+              </span>
+              {/* TODO */}
+              {/* <img src={profile_picture} class="w-6 h-6 rounded-full" alt="profile_picture" /> */}
+              <span
+                class="text-xl font-display hidden lg:flex"
+              >
+                Profile
+              </span>
+            </A>
+            <A
+              href="/scan"
+              class="
+                hidden items-center justify-center rounded-full text-white bg-teal-400 shadow-[0_8px_28px_rgba(0,0,0,0.08)]
+                md:flex md:w-12 md:h-12 lg:w-auto md:p-1 md:my-4 lg:py-3 lg:px-16 hover:bg-teal-500 transition duration-300
+              "
+              activeClass="material-symbols-filled"
+            >
+              <span
+                class="material-symbols-outlined self-center lg:hidden
+                "
+              >
+                barcode_scanner
+              </span>
+              <span
+                class="text-xl font-display hidden lg:flex"
+              >
+                Scan
+              </span>
+            </A>
+          </nav>
+        </div>
+      </div>
+    </div>
+  )
+}
