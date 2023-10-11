429fa17e3146613033f8f63c246cb5266711d83f
Audrey Santoso <104437268+fruitiecutiepie@users.noreply.github.com>
2023-10-11 21:52:31 +1100

    initialise firebase admin

---
+++ 4_value/backend/firebase_admin.ts
@@ -0,0 +1,14 @@
+import admin, { ServiceAccount } from 'firebase-admin';
+
+// Load service account key
+import serviceAccount from '../../2_admin/security/keys/firebase_admin_config.json';
+
+// Initialize the app with a service account
+admin.initializeApp({
+  credential: admin.credential.cert(<ServiceAccount>serviceAccount),
+});
+
+// Accessing different Firebase services:
+export const db = admin.firestore();
+export const auth = admin.auth();
+export const storage = admin.storage();
