24017bb2ee6cc949c19864a402c2339861547d84
Audrey Santoso <104437268+fruitiecutiepie@users.noreply.github.com>
2023-10-11 20:47:44 +1100

    add init data & function

---
+++ 4_value/backend/db/init.ts
@@ -0,0 +1,25 @@
+import brands from './meta.item.brand.json';
+import categories from './meta.item.category.json'
+import items from './item.json'
+import { db } from '../firebase_admin'
+
+(() => {
+  const batch = db.batch();
+
+  batch.set(db.collection('meta').doc('item'), {
+    brands: brands,
+    categories: categories
+  });
+
+  items.forEach((item) => {
+    batch.set(db.collection('item').doc(), item);
+  })
+
+  batch.commit()
+    .then(() => {
+      console.log("Database initialised successfully!");
+    })
+    .catch(error => {
+      console.error("Error initialiasing database: ", error);
+    });
+})();
