eb724a200cccfd85c68f319b5b83b2647f9783e2
Audrey Santoso <104437268+fruitiecutiepie@users.noreply.github.com>
2023-10-15 22:00:15 +1100

    enable offline persistence for web

--- 4_value/gui/src/common/firebaseClientInit.ts
+++ 4_value/gui/src/common/firebaseClientInit.ts
@@ -1,10 +1,10 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeFirestore, persistentLocalCache } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 import firebaseConfig from '../../../../2_admin/security/keys/firebase_config.json'
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-export const db = getFirestore(firebaseApp);
+export const db = initializeFirestore(firebaseApp, {localCache: persistentLocalCache()});
 export const auth = getAuth(firebaseApp);
